b4f73c83f924a67e491194be274c2ffda5fa0a66
Alex Sulino <alexsulino@gmail.com>
2022-02-21 19:50:55 -0300

    Subindo Repositório

---
+++ build/precache-manifest.1e397e46fdb87fd9e8a46f184b1a70c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a388f51736c919a31a7b53245eedbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84bda08bfb4442cd077c",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "84bda08bfb4442cd077c",
+    "url": "/static/js/2.7b6fa548.chunk.js"
+  },
+  {
+    "revision": "61c8745c0501dc4f0c59",
+    "url": "/static/js/main.2c010d29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
